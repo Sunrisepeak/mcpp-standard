c3f92fa697cd37ee22997ec35ad573454c2e16b9
Ankali-Aylina <Ankali_Aylina@qq.com>
2025-06-25 21:10:42 +0800

    修复emoji表情显示错误，添加过时语法检查。

--- dslings/xmake.lua
+++ dslings/xmake.lua
@@ -1,6 +1,15 @@
 add_includedirs(".")
 
+-- 设置编码格式为 utf-8 修复emoji表情错误
+set_encodings("source:utf-8", "target:utf-8")
+
 target("00-0-hello-mcpp")
+    if is_host("windows") then
+        add_cxxflags("/W4 ", {force = true})  -- MSVC 不支持-Wpedantic -Werror，修改为 /W4 /WX
+        -- add_cxxflags("/WX", {force = true})  --严格模式？（笑）
+    else
+        add_cxxflags("-Wpedantic", {force = true})  -- GCC/Clang flags
+        -- add_cxxflags("-Werror", {force = true})  -- 严格模式？（笑）
+    end
     add_files("hello-mcpp.cpp")
 
-includes("cpp11")
