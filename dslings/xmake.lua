daffc427217a500530f395f9ed75d534f29f1243
Ankali-Aylina <Ankali_Aylina@qq.com>
2025-06-25 21:26:57 +0800

    修复emoji表情显示异常的错误，添加了过时语法检查。

--- dslings/xmake.lua
+++ dslings/xmake.lua
@@ -7,9 +7,12 @@ target("00-0-hello-mcpp")
     if is_host("windows") then
         add_cxxflags("/W4 ", {force = true})  -- MSVC 不支持-Wpedantic -Werror，修改为 /W4 /WX
         -- add_cxxflags("/WX", {force = true})  --严格模式？（笑）
+        set_languages("c++14")
     else
         add_cxxflags("-Wpedantic", {force = true})  -- GCC/Clang flags
         -- add_cxxflags("-Werror", {force = true})  -- 严格模式？（笑）
+        set_languages("c++11")
     end
     add_files("hello-mcpp.cpp")
 
+includes("CPP11")
