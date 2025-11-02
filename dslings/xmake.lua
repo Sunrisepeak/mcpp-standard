4ddcf9536585665910d0f68558ed9d3f2066f51f
sunrisepeak <speakshen@163.com>
2025-11-02 17:00:24 +0800

    optimize lang config to fix issue on windows

--- dslings/xmake.lua
+++ dslings/xmake.lua
@@ -1,12 +1,8 @@
-local dslings_dir = os.scriptdir()
-
-includes(path.join(dslings_dir, "d2x/common.lua"))
-
 if is_host("windows") then
     set_encodings("source:utf-8", "target:utf-8")
 end
 
-if get_local_lang() == "zh" then
+if mcpp.lang == "zh" then
     add_includedirs(".")
 
     target("00-0-hello-mcpp")
